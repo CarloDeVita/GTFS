d5b98bc3886c806eb794ef4192e4629d37057ce1
Riccardo Grieco <riccardo.grieco@hotmail.com>
2017-12-04 12:23:21 +0100

    Added hibernate demo

---
+++ src/main/java/demo/hibernate/DemoHibernate.java
@@ -0,0 +1,96 @@
+package demo.hibernate;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import demo.TripDAO;
+import gtfs.FeedParser;
+import gtfs.entities.Trip;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.prefs.Preferences;
+import javax.swing.JFileChooser;
+import com.vividsolutions.jts.geom.MultiPoint;
+import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.PrecisionModel;
+import gtfs.Feed;
+import gtfs.entities.Route;
+import java.util.HashSet;
+
+/**
+ *
+ */
+public class DemoHibernate {
+    public static void main(String[] args) throws IOException {
+        HibernateUtil hibernateUtil = null;
+        try{
+            HibernateUtil.setDefaultCatalog("postgis_test");
+            hibernateUtil = HibernateUtil.getInstance();
+            //if(true) return;
+            Preferences pref = Preferences.userNodeForPackage(DemoHibernate.class);
+            String dirpath = pref.get("gtfs_dir", System.getProperty("user.home"));
+            JFileChooser ch = new JFileChooser();
+            ch.setDialogTitle("Choose directory GTFS");
+            ch.setCurrentDirectory(new File(dirpath));
+            ch.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            ch.requestFocus();
+            if(ch.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+                File currentDir = ch.getCurrentDirectory();
+                pref.put("gtfs_dir", currentDir.getAbsolutePath());
+                File file = ch.getSelectedFile();
+                dirpath = file.getAbsolutePath();
+                FeedParser feedParser = new FeedParser();
+                long start = System.currentTimeMillis();
+                Feed feed = feedParser.read(dirpath);
+                if(false)
+                    throw new FileNotFoundException();
+                long end = System.currentTimeMillis();
+                System.out.println("Ottenuto feed in tempo : "+ ((end-start)/1000.)+" secondi");
+                hibernateUtil.saveOrUpdateCollection(feed.getAgencies());
+                System.out.println("|------------ Saved agencies-----------|");
+                hibernateUtil.saveOrUpdateCollection(feed.getRoutes());
+                System.out.println("|------------ Saved routes-----------|");
+                hibernateUtil.saveOrUpdateCollection(feed.getTrips());
+                System.out.println("|------------ Saved trips-----------|");
+                if(feed.getShapes()!=null)
+                    hibernateUtil.saveOrUpdateCollection(feed.getShapes());
+                System.out.println("|------------ Saved shapes-----------|");
+                hibernateUtil.saveOrUpdateCollection(feed.getStops());
+                System.out.println("|------------ Saved stops-----------|");
+                hibernateUtil.saveOrUpdateCollection(feed.getStopTimes());
+                System.out.println("|------------ Saved stop times-----------|");
+                if(feed.getFrequencies()!=null)
+                hibernateUtil.saveOrUpdateCollection(feed.getFrequencies());
+                System.out.println("|------------ Saved frequencies-----------|");
+                hibernateUtil.saveOrUpdateCollection(feed.getCalendars());
+                System.out.println("|------------ Saved calendars-----------|");
+            }
+            GeometryFactory factory;
+                factory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326);
+
+                TripDAO tripDAO = new TripDAO();
+                Coordinate coord[] = new Coordinate[1];
+                coord[0] = new Coordinate(14.19431, 40.82409);
+                Point point = factory.createPoint(coord[0]);
+                MultiPoint points = factory.createMultiPoint(coord);
+                List<Trip> trips;
+                trips = tripDAO.getTripsForPoints(point, 20);
+                
+                HashSet<Route> routes = new HashSet<>();
+                for(Trip t : trips)
+                    routes.add(t.getRoute());
+                
+                for(Route r : routes)
+                    System.out.println(r.getName());
+        }catch(FileNotFoundException e){
+            System.err.println("ERRORE : "+e.getMessage());
+            throw e;
+        }
+        finally{
+            if(hibernateUtil!=null)
+                hibernateUtil.close();
+        }
+        return;
+    }
+}
---
+++ src/main/java/demo/hibernate/HibernateUtil.java
@@ -0,0 +1,92 @@
+package demo.hibernate;
+
+import java.util.Collection;
+import java.util.Properties;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ * Creates the session factory from the standard (hibernate.cfg.xml) file.
+ */
+public class HibernateUtil {
+    private static HibernateUtil instance;
+    private SessionFactory sessionFactory;
+    private static String defaultCatalog;
+    
+    /**
+     * Sets the default catalog for the session factory.
+     * 
+     * @param catalog the default catalog to set. Null is ignored.
+     */
+    public synchronized static void setDefaultCatalog(String catalog){
+        if(catalog==null) return;
+        defaultCatalog = catalog;
+    }
+    
+    public static synchronized HibernateUtil getInstance(){
+        if(instance==null)
+            instance = new HibernateUtil();
+        return instance;
+    }
+    
+    private HibernateUtil(){
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            Configuration config = new Configuration().configure();
+            if(defaultCatalog!=null)
+                config.setProperty("hibernate.default_catalog", defaultCatalog);
+            sessionFactory = config.buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    
+    public void saveOrUpdateObject(Object object){
+        try (Session session = openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.saveOrUpdate(object);
+            tx.commit();
+        }
+    }
+    
+    public void saveOrUpdateCollection(Collection<?> c){
+        Session session = openSession();
+        int count = 0;
+        Transaction tx = session.beginTransaction();
+        for(Object o : c){
+            if(count==1000){
+                count = 0;
+                tx.commit();
+                session.close();
+                session = openSession();
+                tx = session.beginTransaction();
+            }
+            session.saveOrUpdate(o);
+            count++;
+        }
+        tx.commit();
+        session.close();
+    }
+
+    public Session openSession() {
+        return sessionFactory.openSession();
+    }
+    
+    public void close(){
+        if(sessionFactory!=null)
+            sessionFactory.close();
+        StandardServiceRegistryBuilder.destroy(sessionFactory.getSessionFactoryOptions().getServiceRegistry());
+    }
+}
