d0c7ec223971771159dca9a825c68058d149381b
Carlo <carlo.devita96@gmail.com>
2017-12-18 13:48:14 +0100

    Script sql match

--- src/main/resources/script sql/match.sql
+++ src/main/resources/script sql/match.sql
@@ -1,21 +1,48 @@
-create or replace function matchSeg () returns void as 
-$$
+-- FUNCTION: public.matchseg()
+
+-- DROP FUNCTION public.matchseg();
+
+CREATE OR REPLACE FUNCTION public.matchseg(
+	)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    ROWS 0
+AS $BODY$
+
     declare
-    	sh_cur cursor for select* from gtfs.shape_points ORDER BY shape_id, sequencenumber;
+    	sh_cur cursor for select * from gtfs.shape_points ORDER BY shape_id, sequencenumber;
         seg_id INTEGER;
         last_seg INTEGER;
         lastCoordinate geometry;
         lastShape TEXT;
         angle float;
+        i INTEGER;
+        --
+        p INTEGER;--REFCURSOR;--pgr_costResult;
+        patharray pgr_costResult[];
+        lastsource INTEGER;
+        lasttarget INTEGER;
+        currsource INTEGER;
+        currtarget INTEGER;
+        seg geometry;
     begin
-    	lastCoordinate=null;
         lastShape=null;
         last_seg=null;
     	for sh_rec in sh_cur loop
             IF(lastShape IS NULL OR lastShape<>sh_rec.shape_id) THEN 
                 lastShape:= sh_rec.shape_id;
+                i := 1;
+                last_seg:= null;
+                lastCoordinate:=null;
+                lastsource=null;
+                lasttarget=null;
+                currsource=null;
+                currtarget=null;
 
-                select s.id INTO seg_id
+                select s.id,S.source, S.target INTO seg_id,currsource,currtarget
                 FROM segments S
                 WHERE ST_DWithin(sh_rec.coordinate, S.segment, 0.0001)
                 ORDER BY St_Distance(sh_rec.coordinate, S.segment)
@@ -24,7 +51,7 @@ $$
             	select degrees(st_azimuth(lastCoordinate,sh_rec.coordinate))
                 into angle;
             	
-                select S2.id INTO seg_id
+                select S2.id, S2.source, S2.target, S2.segment INTO seg_id, currsource, currtarget, seg
                 FROM (Select * FROM segments S
                       WHERE ST_DWithin(sh_rec.coordinate, S.segment, 0.0001)
                       ORDER BY St_Distance(sh_rec.coordinate, S.segment) ASC
@@ -41,11 +68,32 @@ $$
             	LIMIT 1;
             END IF;
             
-            IF(last_seg<>seg_id) THEN
-                INSERT into matchedsegments(segment, shape_id, sequencenumber) values(seg_id,sh_rec.shape_id,sh_rec.sequencenumber);
+            IF(last_seg IS NULL OR last_seg<>seg_id) THEN
+                IF(last_seg IS NOT NULL) THEN
+                    seg := ST_MakeLine(lastCoordinate, ST_StartPoint(seg));
+                    /*patharray := pgr_astar('SELECT id,source,target,meters, ST_X(ST_StartPoint(segment)), ST_Y(ST_StartPoint(segment)), ST_X(ST_EndPoint(segment)), ST_Y(ST_EndPoint(segment)) FROM public.segments
+                                            WHERE ST_DWithin(.....,segment)<0.0009',lasttarget ,currsource,false,false);*/
+                    FOR p IN (SELECT edge FROM pgr_astar('SELECT id ,source,target,meters AS cost,CASE WHEN oneway=''true'' THEN -1 ELSE meters END AS reverse_cost, ST_X(ST_StartPoint(segment)) AS x1, ST_Y(ST_StartPoint(segment)) AS y1, ST_X(ST_EndPoint(segment)) AS x2, ST_Y(ST_EndPoint(segment)) AS y2 FROM public.segments
+                                            WHERE ST_DWithin(ST_GeomFromText('''|| ST_AsText(seg) ||''',4326),segment,0.0002) AND source IS NOT NULL AND target IS NOT NULL',lasttarget ,currsource,false))
+                    LOOP
+                        IF(p<>last_seg AND p<>seg_id) THEN
+                            INSERT INTO matchedsegments(segment, shape_id, sequencenumber) values(p,sh_rec.shape_id,i);
+                            i := i+1;
+                        END IF;
+                    END LOOP;
+                END IF;
+                INSERT into matchedsegments(segment, shape_id, sequencenumber) values(seg_id,sh_rec.shape_id,i);
+                i := i+1;
+                last_seg := seg_id;
             END IF;
             lastCoordinate:=sh_rec.coordinate;
+            lastsource:=currsource;
+            lasttarget:=currtarget;
         end loop;
         
     end;
-$$ language plpgsql;
+
+$BODY$;
+
+ALTER FUNCTION public.matchseg()
+    OWNER TO postgres;
